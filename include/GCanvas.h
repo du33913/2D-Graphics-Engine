15a491464a29e7e20ae02e80b6696d34a48e9b81
Kyle <59694785+du33913@users.noreply.github.com>
2022-12-14 23:44:12 -0500

    Add files via upload

--- include/GCanvas.h
+++ include/GCanvas.h
@@ -7,6 +7,8 @@
 
 #include "GMatrix.h"
 #include "GPaint.h"
+#include "GShader.h"
+#include "GPath.h"
 #include <string>
 
 class GBitmap;
@@ -145,6 +147,32 @@ public:
     void fillRect(const GRect& rect, const GColor& color) {
         this->drawRect(rect, GPaint(color));
     }
+
+    /**
+     *  Return a radial-gradient shader.
+     *
+     *  This is a shader defined by a circle with center and a radius.
+     *  The array of colors are evenly distributed between the center (color[0]) out to
+     *  the radius (color[count-1]). Beyond the radius, it respects the TileMode.
+     */
+    virtual std::unique_ptr<GShader> createRadialGradient(GPoint center, float radius,
+                                                          const GColor colors[], int count,
+                                                          GShader::TileMode mode) {
+        return nullptr;
+    }
+
+    enum CapType {
+        kButt,      // no cap on the line
+        kSquare,    // square cap extending width/2
+        kRound,     // round cap with radius = width/2
+    };
+
+    /**
+     *  Add contour(s) to the specified path that will draw a line from p0 to p1 with the specified
+     *  width and CapType. Note that "width" is the distance from one side of the stroke to the
+     *  other, ala its thickness.
+     */
+    virtual void addLine(GPath* path, GPoint p0, GPoint p1, float width, CapType) {}
 };
 
 /**
